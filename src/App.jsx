73e957df3c7c8ab64e4dd3fdcf5e95d69eebe5a7
Zaid Hassan <zaidhassan251@gmail.com>
2024-10-15 15:47:19 +0530

    Add basic signup and dashboard UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,12 @@
+import React from 'react'
+import { Navbar } from './components/components'
 
+function App() {
+    return (
+        <div className="dark">
+            <Navbar />
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/components.js
@@ -0,0 +1,5 @@
+import Signup from "./Signup/Signup";
+import Navbar from "./Navbar/Navbar";
+import Dashboard from "./Dashboard/Dashboard";
+
+export { Signup, Navbar, Dashboard };
