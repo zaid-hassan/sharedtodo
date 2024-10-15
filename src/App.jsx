b4637753e4ad904858391821d380f962a50c5a0c
Zaid Hassan <zaidhassan251@gmail.com>
2024-10-15 17:09:40 +0530

    Add basic React Router Completed basic React Router setup. Dashboard and Singup pages loading and browser routing completed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Navbar } from './components/components'
+import { Outlet } from 'react-router-dom'
 
 function App() {
     return (
         <div className="dark">
             <Navbar />
+            <div className="p-4 sm:ml-64">
+                <Outlet />
+            </div>
         </div>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import { Signup, Dashboard } from './components/components.js'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route path='signup' element={<Signup />} />
+      <Route path='dashboard' element={<Dashboard />} />
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
