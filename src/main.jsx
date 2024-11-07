b6399e4dd0ac505b0722ba2b67f672b86060e107
Zaid Hassan <zaidhassan251@gmail.com>
2024-11-07 15:06:46 +0530

    setup chakraui

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { Provider } from "@/components/ui/provider"
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </StrictMode>,
 )
